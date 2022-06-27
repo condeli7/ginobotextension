7e642e1068d448e2d48c3c2c647bc88e9026201e
condeli7 <constantinos.deli7@gmail.com>
2022-06-27 16:27:28 +0300

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-def on_button_pressed_a():
-    pass
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-testVar = 0
-ginobot.move(MoveDirection.FORWARD, Threshold.LOW)
-ginobot.move_duration(MoveDirection.FORWARD, Threshold.LOW, min(0, 0))
--- main.ts
+++ main.ts
@@ -1,6 +1,1 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-})
-let testVar = 0
-ginobot.move(MoveDirection.Forward, Threshold.Low)
-ginobot.move_duration(MoveDirection.Forward, Threshold.Low, Math.min(0, 0))
+
